a265a1ae08078ffdce12f21bf2c3bf1659df8216
Paolo Dávila <pdavila@iesebre.com>
2017-03-08 19:57:54 +0100

    Example2 OK

--- client/index.js
+++ client/index.js
@@ -1,4 +1,13 @@
 var socket = require('socket.io-client')('http://localhost:3000');
-socket.on('connect', function(){});
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
+
+socket.on('connect', function(){
+    console.log("Client connected OK to localhost port 3000");
+});
+
+socket.on('event', function(data){
+    console.log("Event received!");
+});
+
+socket.on('disconnect', function(){
+    console.log("Client disconnected OK from localhost port 3000");
+});
--- server/standalone.js
+++ server/standalone.js
@@ -2,5 +2,8 @@ var io = require('socket.io')();
 io.on('connection', function(client) {
    //TODO
     console.log("New connection received!");
+    //console.log(client);
+
+    client.emit('event','test!');
 });
 io.listen(3000);
